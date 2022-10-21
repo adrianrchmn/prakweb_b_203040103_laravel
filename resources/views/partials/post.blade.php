ac1f433156b2079cfdb340f915b5e81978688f6c
adrianrchmn <203040103@mail.unpas.ac.id>
2022-10-21 22:23:15 +0700

    Blade Templating EnginE

---
+++ resources/views/partials/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('container')
+    <article>
+    <h2>Judul</h2>
+    <h5>Author</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati vel quae quam magni quia earum deleniti odio possimus blanditiis aspernatur voluptatum pariatur recusandae veniam, iure, qui eius perspiciatis amet inventore.</p>
+    </article>
+
+    <a href="/blog">Back to Posts</a>
+    @endsection
