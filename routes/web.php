426a4fe3530384431592233cc70ebe427d2ece7d
adrianrchmn <203040103@mail.unpas.ac.id>
2022-10-21 17:58:11 +0700

    Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Adrian Rachman",
+        "image" => "1.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
