faea2975beb2c803f2f2eb5d145e5a15ca219c72
adrianrchmn <203040103@mail.unpas.ac.id>
2022-10-30 18:56:08 +0700

    Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
     <h1 class="mb-5">{{ $post->title }}</h1>
     {{-- <h5>{{ $post["author"] }}</h5> --}}
 
+    <p>By. Adrian Rachman in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
     <br>
