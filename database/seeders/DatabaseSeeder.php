6b16fe0cece7cb3d44854e0ef6183372e0610ba7
adrianrchmn <203040103@mail.unpas.ac.id>
2022-10-30 21:36:26 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,62 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Adrian Rachman',
+            'email' => '203040103@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Indra',
+            'email' => 'indra123@gmail.com',
+            'password' => bcrypt('54321')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi.',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi. Nam illo magni beatae, obcaecati animi porro ex et exercitationem, cumque amet, nobis voluptatibus voluptates iste sequi totam culpa blanditiis quasi laborum nesciunt non eaque. Illo saepe veniam laudantium aliquid! Itaque exercitationem fugit quae officiis nam iste fugiat ullam perspiciatis corrupti omnis facilis doloremque quod nisi obcaecati, tempore eius enim ratione aliquam provident corporis error ex adipisci voluptatem? Et tempore temporibus magni porro eaque culpa maxime optio sunt reprehenderit, perspiciatis voluptatem eius laudantium. Dolorum omnis voluptas, dicta quo, nulla ex temporibus quia impedit, assumenda exercitationem similique provident molestiae aliquam?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi.',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi. Nam illo magni beatae, obcaecati animi porro ex et exercitationem, cumque amet, nobis voluptatibus voluptates iste sequi totam culpa blanditiis quasi laborum nesciunt non eaque. Illo saepe veniam laudantium aliquid! Itaque exercitationem fugit quae officiis nam iste fugiat ullam perspiciatis corrupti omnis facilis doloremque quod nisi obcaecati, tempore eius enim ratione aliquam provident corporis error ex adipisci voluptatem? Et tempore temporibus magni porro eaque culpa maxime optio sunt reprehenderit, perspiciatis voluptatem eius laudantium. Dolorum omnis voluptas, dicta quo, nulla ex temporibus quia impedit, assumenda exercitationem similique provident molestiae aliquam?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi.',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi. Nam illo magni beatae, obcaecati animi porro ex et exercitationem, cumque amet, nobis voluptatibus voluptates iste sequi totam culpa blanditiis quasi laborum nesciunt non eaque. Illo saepe veniam laudantium aliquid! Itaque exercitationem fugit quae officiis nam iste fugiat ullam perspiciatis corrupti omnis facilis doloremque quod nisi obcaecati, tempore eius enim ratione aliquam provident corporis error ex adipisci voluptatem? Et tempore temporibus magni porro eaque culpa maxime optio sunt reprehenderit, perspiciatis voluptatem eius laudantium. Dolorum omnis voluptas, dicta quo, nulla ex temporibus quia impedit, assumenda exercitationem similique provident molestiae aliquam?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi.',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, ducimus quasi. Nam illo magni beatae, obcaecati animi porro ex et exercitationem, cumque amet, nobis voluptatibus voluptates iste sequi totam culpa blanditiis quasi laborum nesciunt non eaque. Illo saepe veniam laudantium aliquid! Itaque exercitationem fugit quae officiis nam iste fugiat ullam perspiciatis corrupti omnis facilis doloremque quod nisi obcaecati, tempore eius enim ratione aliquam provident corporis error ex adipisci voluptatem? Et tempore temporibus magni porro eaque culpa maxime optio sunt reprehenderit, perspiciatis voluptatem eius laudantium. Dolorum omnis voluptas, dicta quo, nulla ex temporibus quia impedit, assumenda exercitationem similique provident molestiae aliquam?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,10 +4,9 @@
     <h1 class="mb-5">{{ $post->title }}</h1>
     {{-- <h5>{{ $post["author"] }}</h5> --}}
 
-    <p>By. Adrian Rachman in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
     {!! $post->body !!}
 
-    <br>
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
     @endsection
