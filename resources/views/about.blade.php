b104e6584ea83a0a527187005985d97fce73baa9
adrianrchmn <203040103@mail.unpas.ac.id>
2022-12-15 12:06:54 +0700

    Searching & Pagination

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,17 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman About</h1>
-    <h3>{{ $name }}</h3>
-    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
+    <div class="container mb-5 text-center">
+            <div class="row">
+                <div class="col">
+                    <div class="card mb-5">
+                        <div class="position-relative px-3 py-2" style="background-color: rgba(255, 255, 255, 0.2)">
+                            <h1 >Halaman About</h1>
+                            <h3 >{{ $name }}</h3>
+                            <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
