2195405e3e5056fa290c1377fd3f91ab74743d74
agungSeptiana <203040102@mail.unpas.ac.id>
2022-12-15 10:07:39 +0700

    Menambahkan video 12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman Home</h1>
+    <br>
+    <div class="container mb-5 text-center">
+        <div class="row">
+            <div class="col">
+                <div class="card mb-5">
+                    <div class="position-relative px-3 py-2">
+                        <h2>Halaman Home</h2>
+                        <p>Selamat Datang di Blog Adrian Rachman</p>
+                        <a href="/about" class="btn btn-danger">About Me</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
@@ -28,19 +29,18 @@ Route::get('/about', function () {
     return view('about', [
         "title" => "About",
         "name" => "Adrian Rachman",
-        "image" => "1.png"
+        "image" => "1.png",
+        "active" => "about"
     ]);
 });
 
-
-Route::get('/blog', [PostController::class, 'index']);
-
-// halaman single post
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('post/{post:slug}', [PostController::class, 'show']);
 
 Route::get('/categories', function() {
     return view('categories',[
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -48,6 +48,7 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts
     ]);
 });
@@ -55,6 +56,6 @@ Route::get('/categories/{category:slug}', function(Category $category) {
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author'),
+        'posts' => $author->posts->load('category', 'author')
     ]);
 });
